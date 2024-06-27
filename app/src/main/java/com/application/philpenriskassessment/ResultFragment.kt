fe4e109442ef85b6ba468aedd013cd9d93f32612
alicandroho <icongbrigette11@gmail.com>
2024-06-27 22:33:28 +0800

    not sure

--- app/src/main/java/com/application/philpenriskassessment/ResultFragment.kt
+++ app/src/main/java/com/application/philpenriskassessment/ResultFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.application.philpenriskassessment.databinding.FragmentResultBinding
 
 class ResultFragment : Fragment() {
@@ -17,44 +18,51 @@ class ResultFragment : Fragment() {
     ): View? {
         binding = FragmentResultBinding.inflate(inflater,container,false)
 
-        val age = requireArguments().getInt("age")
-        val sex = requireArguments().getString("sex")
-        val sbp = requireArguments().getInt("sbp")
-        val smoker = requireArguments().getString("smoker")
-        val cholesterol = requireArguments().getInt("cholesterol")
-        val checker =  sex + sbp + smoker + cholesterol + age
+        val bundleAge = requireArguments().getString("age")!!.toInt()
+        val bundleSex = requireArguments().getString("sex")
+        val bundleSbp = requireArguments().getString("sbp")!!.toInt()
+        val bundleSmoker = requireArguments().getString("smoker")
+        val bundleCholesterol = requireArguments().getString("cholesterol")!!.toInt()
+
+        val checker =  bundleSex + bundleAge + bundleSmoker + bundleCholesterol + bundleSbp
         binding.title.text = checker
 
         //algorithm
-        val green = 10
-        val yellow = 20
-        val orange = 30
-        val red = 40
-        val deepRed = 40
+//        if (bundleAge in 40..49 && bundleSmoker == "non-smoker" && bundleSbp in 140..159 && bundleCholesterol in 155..310) {
+//            binding.results.setBackgroundColor(Color.GREEN)
+//        } else if (bundleSex == "Female" && bundleAge in 40..49 && bundleSmoker == "non-smoker" && bundleSbp in 160..179 && bundleCholesterol in 155..310){
+//            if (bundleSex == "Male") {
+//                //orange
+//                binding.results.setBackgroundColor(Color.parseColor("#FFCC99"))
+//            } else {
+//                //dilaw
+//                binding.results.setBackgroundColor(Color.YELLOW)
+//            }
+//        } else if (bundleAge in 40..49 && bundleSmoker == "non-smoker" && bundleSbp >= 180 && bundleCholesterol >= 310) {
+//            //deep red
+//            binding.results.setBackgroundColor(Color.parseColor("#C70039"))
+//        } else if (bundleAge in 40..59 && bundleSmoker == "non-smoker" && bundleSbp in 120..139 && bundleCholesterol >= 310) {
+//            binding.results.setBackgroundColor(Color.GREEN)
+//        } else if (bundleAge in 40..59 && bundleSmoker == "non-smoker" && bundleSbp in 140..159) {
+//            if (bundleCholesterol >= 310) {
+//                if (bundleSex == "Male") {
+//                    binding.results.setBackgroundColor(Color.YELLOW)
+//                } else {
+//                    binding.results.setBackgroundColor(Color.GREEN)
+//                }
+//            }
+//        } else if (bundleAge in 40..59 && bundleSmoker == "non-smoker" && bundleSbp in 140..159 && bundleCholesterol in 271..309) {
+//            binding.results.setBackgroundColor(Color.GREEN)
+//        } else if (bundleAge in 40..59 && bundleSmoker == "non-smoker" && bundleSbp in 160..179 && bundleCholesterol in 271..309) {
+//            if (bundleSex == "Male") {
+//                binding.results.setBackgroundColor(Color.YELLOW)
+//            } else {
+//                binding.results.setBackgroundColor(Color.GREEN)
+//            }
+//        } else if ()
 
-        val patientCalculation = age + cholesterol + sbp
-        if (sex == "Male") {
-            if (smoker == "smoker"){
-                if (patientCalculation < green) {
-                    binding.results.setBackgroundColor(Color.GREEN)
-                } else if (patientCalculation < yellow) {
-                    binding.results.setBackgroundColor(Color.YELLOW)
-                } else if (patientCalculation < orange) {
-                    binding.results.setBackgroundColor(Color.parseColor("#FFCC99"))
-                } else if (patientCalculation < red) {
-                    binding.results.setBackgroundColor(Color.RED)
-                } else if (patientCalculation > deepRed) {
-                    binding.results.setBackgroundColor(Color.parseColor("#C70039"))
-                }
-            } else {
-                Toast.makeText(context, "non-smoker/male", Toast.LENGTH_SHORT).show()
-            }
-        } else {
-            if (smoker == "smoker"){
-                Toast.makeText(context, "smoker/female", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(context, "non-smoker/female", Toast.LENGTH_SHORT).show()
-            }
+        binding.home.setOnClickListener {
+            findNavController().navigate(R.id.action_resultFragment_to_referFragment)
         }
 
         return binding.root
